b6dbec13a3b6136757b482f59627c20d12df098b
Lusehair <56312971+lusehair@users.noreply.github.com>
2022-01-30 23:43:54 +0100

    :rotating_light: USER command implemented, not tested

--- src/Server.commands.cpp
+++ src/Server.commands.cpp
@@ -23,6 +23,7 @@ void irc::Server::cmd_nick(void *sender)
     std::string nick; 
     std::string tmp(_main_buffer);
     std::size_t found = tmp.find("NICK") + 4; 
+
     tmp.copy((char*)nick.c_str(), tmp.size() - found , found);
     std::map<std::string, irc::User>::iterator connected_it = _connected_users.find(nick); 
     std::map<int, std::string>::iterator unnamed_it;
@@ -51,4 +52,45 @@ void irc::Server::cmd_nick(void *sender)
 }
 
 
+/**
+ * @brief command USER from IRC Protocl
+ * 
+ * We have 2 cases : 
+ * 1. If the USER command doesn't have username on his request 
+ * 2. User is already register 
+ * 
+ * @param input_fd (int)
+ * @sa RFC 2812 (3.1.3)
+ * 
+ */
+void irc::Sevrer::cmd_user(void *input_fd)
+{
+    const int fd = *(reinterpret_cast<int*>(sender)); 
+    std::string username; 
+    std::string tmp(_main_buffer);
+    std::size_t start = tmp.find("USER") + 5; 
+    std::size_t nb_of_space = std::count(tmp.begin(), tmp.end(), ' ');
+    if(nb_of_space == 4)
+    {
+        send(fd, 461, sizeof(int), MSG_DONTWAIT)
+        return ; 
+    }
+    tmp.copy((char*)user.c_str(), start , end - start);
+    std::map<int, std::string>::iterator unnamed_it; 
 
+
+    if(connected_it != _unnamed_users.end())
+    {
+        std::map<int, std::string>::iterator check_even_connected_it = _connected_users.find(connected_it->second); 
+        if(check_even_conneceted_it != _connected_users.end())
+        {
+            send(fd, 462, sizeof(int), MSG_DONTWAIT)
+            return ;
+        }
+        else if(connected_it->second.size() != 0)
+        {
+            _connected_users.insert(User(_connected_it->second, username, fd));
+            _unnamed_users.erase(fd);
+        }
+    }
+}
